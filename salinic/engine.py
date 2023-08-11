09416a039301e1f8c4663062af84e6c8fbaf9b05
Eugen Ciur <eugen@papermerge.com>
2023-08-11 09:03:15 +0200

    add mode flag (RW/RO)

--- salinic/engine.py
+++ salinic/engine.py
@@ -1,18 +1,28 @@
+from enum import Enum
+
 import xapian
 
 from .utils import get_db_path
 
 
+class AccessMode(Enum):
+    RO = 1  # read only
+    RW = 2  # read/write
+
+
 class Engine:
 
-    def __init__(self, dsn: str):
+    def __init__(self, dsn: str, mode: AccessMode = AccessMode.RO):
         self._dsn = dsn
         self._db_path = get_db_path(dsn)
-        self._db = xapian.WritableDatabase(
-            self._db_path,
-            xapian.DB_CREATE_OR_OPEN
-        )
+        if mode == AccessMode.RO:
+            self._db = xapian.Database(self._db_path)
+        else:  # mode == AccessMode.RW
+            self._db = xapian.WritableDatabase(
+                self._db_path,
+                xapian.DB_CREATE_OR_OPEN
+            )
 
 
-def create_engine(dsn: str) -> Engine:
-    return Engine(dsn)
+def create_engine(dsn: str, mode: AccessMode = AccessMode.RO) -> Engine:
+    return Engine(dsn, mode=mode)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from salinic.engine import create_engine
+from salinic.engine import AccessMode, create_engine
 from salinic.session import Session
 
 
@@ -8,6 +8,6 @@ from salinic.session import Session
 def session(tmp_path) -> Session:
     d = tmp_path / "index_db"
     d.mkdir()
-    engine = create_engine(f"xapian://{d}")
+    engine = create_engine(f"xapian://{d}", mode=AccessMode.RW)
 
     return Session(engine)
