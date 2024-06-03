e4fb1cf09193b5a6693940d3ccd9b9e808e12037
Eugen Ciur <eugen@papermerge.com>
2024-06-03 08:00:25 +0200

    couple of more unit tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -4,4 +4,4 @@ from salinic.engine import create_engine
 def test_engine():
     engine = create_engine('solr://solr-instance.default')
     assert engine.url.scheme == 'solr'
-    assert str(engine.url.scheme) == 'solr'
+    assert str(engine.url.scheme.value) == 'solr'
